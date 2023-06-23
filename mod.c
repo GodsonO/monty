5065eb5a789693126b784bcc998f1aeb37f54fa7
GodsonO <godsonomoghene15@gmail.com>
2023-06-22 19:00:40 -0700

    stacks queues

--- mod.c
+++ mod.c
@@ -3,23 +3,23 @@
  * f_mod - computes the rest of the division of the second
  * top element of the stack by the top element of the stack
  * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @count_line: number of lines
+ * Return: void
 */
-void f_mod(stack_t **head, unsigned int counter)
+void f_mod(stack_t **head, unsigned int count_line)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int length = 0, _aux;
 
 	h = *head;
 	while (h)
 	{
 		h = h->next;
-		len++;
+		length++;
 	}
-	if (len < 2)
+	if (length < 2)
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", count_line);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
@@ -28,14 +28,14 @@ void f_mod(stack_t **head, unsigned int counter)
 	h = *head;
 	if (h->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
+		fprintf(stderr, "L%d: division by zero\n", count_line);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n % h->n;
-	h->next->n = aux;
+	_aux = h->next->n % h->n;
+	h->next->n = _aux;
 	*head = h->next;
 	free(h);
 }
